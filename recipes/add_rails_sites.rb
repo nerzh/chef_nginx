fd7e92f7adbb79f7feae4176db2625c46cf97db8
woodcrust <roboucrop@gmail.com>
2017-08-28 14:26:04 +0300

    fix dynamic attributes

--- recipes/add_rails_sites.rb
+++ recipes/add_rails_sites.rb
@@ -1,11 +1,14 @@
 node['chef_nginx']['sites'].each do |sites|
   sites['apps'].keys.each do |site|
     chef_nginx_add_rails_sites site do
-      root_path     sites['root_path']
+      root_path     node['chef_nginx']['root_path'] unless sites['root_path']
       user          sites['user']
-      projects_path sites['projects_path']
+      projects_path node['chef_nginx']['projects_path'] unless sites['projects_path']
       ssl_exist     sites['apps'][site]['ssl_exist']
-      cert_path     sites['apps'][site]['cert_path']
+      cert_path     sites['apps'][site]['cert_path']    if sites['apps'][site]['cert_path']
+      current_path  sites['apps'][site]['current_path'] if sites['apps'][site]['current_path']
+      socket_path   sites['apps'][site]['socket_path']  if sites['apps'][site]['socket_path']
+      static_path   sites['apps'][site]['static_path']  if sites['apps'][site]['static_path']
       action :add
     end
   end
--- resources/add_rails_sites.rb
+++ resources/add_rails_sites.rb
@@ -4,16 +4,23 @@ actions :add
 default_action :add
 
 property :name,          String, name_attribute: true
-property :root_path,     String
+property :root_path,     String, required: true
 property :user,          String, required: true
-property :projects_path, String
+property :projects_path, String, required: true
 property :ssl_exist,     [String, TrueClass, FalseClass], default: false
-property :cert_path,     String
+property :cert_path,     [String, NilClass], default: nil
 property :current_path,  [String, NilClass], default: nil
+property :socket_path,   [String, NilClass], default: nil
+property :static_path,   [String, NilClass], default: nil
 
 action :add do
-  app_dir = "/#{new_resource.root_path}/#{new_resource.user}/#{new_resource.projects_path}/#{new_resource.name}"
-  app_dir += "/#{new_resource.current_path}" if new_resource.current_path
+  app_dir     = "/#{new_resource.root_path}/#{new_resource.user}/#{new_resource.projects_path}/#{new_resource.name}"
+  socket_path = "/#{app_dir}/shared/shared/puma.sock"
+  static_path = "/#{app_dir}/public"
+  
+  app_dir     += new_resource.current_path if new_resource.current_path
+  socket_path += new_resource.socket_path if new_resource.socket_path
+  static_path += new_resource.static_path if new_resource.static_path
   
   execute "restart-nginx" do
     command "bash -lc 'service nginx restart'"
@@ -35,7 +42,9 @@ action :add do
     mode   '0644'
     variables props: {
       "name" => new_resource.name, 
-      "app_dir" => app_dir, 
+      "app_dir" => app_dir,
+      "socket_path" => socket_path,
+      "static_path" => static_path,
       "ssl_exist" => new_resource.ssl_exist,
       "crt" => "ssl_certificate /etc/nginx/ssl/#{new_resource.name}.crt;",
       "key" => "ssl_certificate_key /etc/nginx/ssl/#{new_resource.name}.key;"
