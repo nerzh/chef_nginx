87ad7bdcc1c76fd07cbac6b15e73af6b5560a209
woodcrust <roboucrop@gmail.com>
2017-08-28 14:42:09 +0300

    fix operator

--- recipes/add_rails_sites.rb
+++ recipes/add_rails_sites.rb
@@ -1,8 +1,8 @@
 node['chef_nginx']['sites'].each do |sites|
   sites['apps'].keys.each do |site|
     chef_nginx_add_rails_sites site do
-      sites['root_path']     ? root_path sites['root_path']         : root_path node['chef_nginx']['root_path']
-      sites['projects_path'] ? projects_path sites['projects_path'] : projects_path node['chef_nginx']['projects_path']
+      sites['root_path']     ? root_path(sites['root_path'])         : root_path(node['chef_nginx']['root_path'])
+      sites['projects_path'] ? projects_path(sites['projects_path']) : projects_path(node['chef_nginx']['projects_path'])
       user          sites['user']
       ssl_exist     sites['apps'][site]['ssl_exist']
       cert_path     sites['apps'][site]['cert_path']    if sites['apps'][site]['cert_path']
