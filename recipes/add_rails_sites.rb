21695c8de5b6fd4f89e040e7318f2808fbc4c6b9
woodcrust <roboucrop@gmail.com>
2017-08-28 14:39:45 +0300

    fix required attribute

--- recipes/add_rails_sites.rb
+++ recipes/add_rails_sites.rb
@@ -1,9 +1,9 @@
 node['chef_nginx']['sites'].each do |sites|
   sites['apps'].keys.each do |site|
     chef_nginx_add_rails_sites site do
-      root_path     node['chef_nginx']['root_path'] unless sites['root_path']
+      sites['root_path']     ? root_path sites['root_path']         : root_path node['chef_nginx']['root_path']
+      sites['projects_path'] ? projects_path sites['projects_path'] : projects_path node['chef_nginx']['projects_path']
       user          sites['user']
-      projects_path node['chef_nginx']['projects_path'] unless sites['projects_path']
       ssl_exist     sites['apps'][site]['ssl_exist']
       cert_path     sites['apps'][site]['cert_path']    if sites['apps'][site]['cert_path']
       current_path  sites['apps'][site]['current_path'] if sites['apps'][site]['current_path']
