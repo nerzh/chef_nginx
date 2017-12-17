a6f57da307d5eaf641e19183f9a8a7f5bfc3aad1
woodcrust <roboucrop@gmail.com>
2017-12-17 09:20:52 +0200

    fix name

--- recipes/add_front_static_sites.rb
+++ recipes/add_front_static_sites.rb
@@ -1,6 +1,6 @@
 node['chef_nginx']['sites'].each do |sites|
   sites['apps'].keys.each do |site|
-    chef_nginx_add_rails_sites site do
+    chef_nginx_add_front_static_sites(site) do
       sites['root_path']     ? root_path(sites['root_path'])         : root_path(node['chef_nginx']['root_path'])
       sites['projects_path'] ? projects_path(sites['projects_path']) : projects_path(node['chef_nginx']['projects_path'])
       user          sites['user']
